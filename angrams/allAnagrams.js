ac7e210359f0ca2e0e6705ba09a3139c9bca9163
rwaida <rwaidaAlmehanni>
2017-01-01 10:25:50 +0200

    toy2

--- angrams/allAnagrams.js
+++ angrams/allAnagrams.js
@@ -14,8 +14,56 @@
   * var anagrams = allAnagrams('abc');
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
-
+//______________________________________________________
 var allAnagrams = function(string) {
-	
-
+	var t="",arr=[],l=string.length,a=string.split("");
+	for(var i=0;i<l*2;i++){
+		if(i%2===0){
+           t=a[0];
+           a[0]=a[1];
+           a[1]=t;
+			arr.push(a.join(""))
+		}
+		else{
+			t=a[l-1];
+			a[l-1]=a[l-2];
+			a[l-2]=t;
+			arr.push(a.join(""));
+		}
+	}
+	return arr.sort();
 };
+//_____________________________________________________
+function convert(arr){
+	var t="",result=arr.join(""),l=arr.length;
+	for(var i=1;i<l-1;i++){
+		t=arr[i];
+		arr[i]=arr[l-1];
+		arr[l-1]=t;
+        result+=arr.join("")+" ";
+	}
+	return result.slice(0,-1);
+};
+function allAnagrams2(string){
+	var arr=string.split(""),result=[],s="",a=[];
+	var l=arr.length;
+	s=convert(arr);
+	a=s.split(" ");
+    for(var i=0;i<a.length;i++){
+         	result.push(a[i]);
+         }
+	for(var j=1;j<l;j++){
+         t=arr[j];
+         arr.splice(j,1);
+         arr.unshift(t);
+         s=convert(arr);
+         a=s.split(" ");
+         for(var i=0;i<a.length;i++){
+         	result.push(a[i]);
+         }
+         
+	}
+	return result.sort();
+
+}
+//_________________________________________
