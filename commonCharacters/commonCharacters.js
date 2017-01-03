0678e1a6f58abd359deeaaef615866708a632764
rwaida <rwaidaAlmehanni>
2017-01-03 09:43:28 +0200

    commonToy

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -12,6 +12,35 @@
 
 //Works for any number of input strings:
 var commonCharacters = function(string1, string2) {
+	var arr=string1.split("");
+	return arr.reduce(function(acc,val){
+		if(val!==" "){
+		if(string2.indexOf(val)!==-1){
+			acc+=val;
+		}
+	}
+	return acc;
+	},"")
   
 };
+//extra......................................
+var commonCharactersExtra = function(string1, string2) {
+	var arg=Array.from(arguments);
+	var arr=arg[0].split("");
+	var f=true;
+	return arr.reduce(function(acc,val){
+		   f=true;
+		if(val!==" "){
+			for(var i=1;i<arg.length;i++){
+		       if(arg[i].indexOf(val)===-1){
+			      f=false;
+		         }
+	          }
+	      if(f){acc+=val;}
+          }
+	return acc;
+	},"")
+  
+};
+//_______________________________________
 
