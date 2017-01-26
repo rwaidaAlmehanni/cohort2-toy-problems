f9ebb8cb32db5973cfa943cd9d0bf8afb3ffc2b4
rwaida <rwaidaAlmehanni>
2017-01-26 08:36:14 +0200

    nnn

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -10,51 +10,52 @@
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
 
-var spiralTraversal = function(matrix){
-	var arr=matrix[0],j=matrix.length-1,n=1;
-	while(n<matrix.length-1){
-	for(var i=1;i<matrix.length;i++){
-		if(i!==j){
-      arr.push(matrix[i][j])
-	}else{
-		for(var d=matrix[i].length-1;d>=0;d--){
-			arr.push(matrix[i][d])
-		}
-		j--;
-	}
-}
-n++;
-}
-return arr;
-};
-var spiralTraversal2 = function(matrix){
-	var arr=matrix[0],j=matrix.length-1,n=1,f=true;
-	while(n<matrix.length-1){
-	if(f){
-	     for(var i=1;i<matrix.length;i++){
-		    if(i!==j){
-            arr.push(matrix[i][j])
-            matrix.splice([i][j],1)
-	       }else{
-		      for(var d=matrix[i].length-1;d>=0;d--){
-			      arr.push(matrix[i][d])
-			     matrix.splice(matrix[i][d])
-		            }
-		         j--;
-	           }
-            }
-    f=false;
-    }else{ 
-    	x=0;
-	    for(var g=0;g<matrix.length;g++){
-		   arr.push(matrix[g][x])
-		     x++;
-	      }
-       }
-       n++;
-   }
-return arr;
-};
+// var spiralTraversal = function(matrix){
+// 	var arr=matrix[0],j=matrix.length-1,n=1;
+// 	while(n<matrix.length-1){
+// 	for(var i=1;i<matrix.length;i++){
+// 		if(i!==j){
+//       arr.push(matrix[i][j])
+// 	}else{
+// 		for(var d=matrix[i].length-1;d>=0;d--){
+// 			arr.push(matrix[i][d])
+// 		}
+// 		j--;
+// 	}
+// }
+// n++;
+// }
+// return arr;
+// };
+// var spiralTraversal2 = function(matrix){
+// 	var arr=[],f=1,j=matrix.length-1;
+// 	for(var i=0;i<matrix.length;i++){
+// 		if(f===1){
+// 			for(var e=0;e<matrix.length;e++){
+// 				arr.push(matrix[i][j])
+// 			}
+// 			f=2;
+// 		}
+// 		else if(f===2){
+// 			for(var k=i;k<matrix.length-i;k++){
+// 				arr.push(matrix[k][j])
+// 			}
+// 			f=3;
+// 			j--;
+// 		}
+// 		else if(f===3){
+// 			for(var e=j;e>=0;e--){
+// 				arr.push(matrix[j][e])
+// 			}
+// 			f=4;
+// 		}
+// 		else if(f===4){
+// 			for(var h=)
+// 		}
+// 	}
+
+// return arr;
+// };
 // var spiralTraversal = function(matrix){
 // 	var arr=matrix[0],j=matrix.length-1,n=1,f=1;
 	
