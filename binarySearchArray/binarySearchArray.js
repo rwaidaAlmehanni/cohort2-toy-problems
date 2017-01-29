0ac136ca713573e56f53b6e306fffe916f8837cb
rwaida <rwaidaAlmehanni>
2017-01-29 09:00:35 +0200

    direction

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,24 +10,24 @@
  * console.log(index); // 4
  */
 
-var binarySearch = function (array, target) {
-	var index=Math.floor(array.length/2);
-	if( target === array[index] ){
-		return index;
-	}else if(target > array[index]){
-		       for(var i=array[index]+1 ; i<array.length ; i++){
-			        if(target === array[i]){
-				         return i;
-			         }
-		       }
-	}else{
-		   for(var i=0 ; i<index ; i++){
-			   if(target === array[i]){
-				   return i;
-			   }
-		}
-	}
-return "not found";  
-};
+ var binarySearch = function (array, target) {
+ 	var index=Math.floor(array.length/2);
+ 	if( target === array[index] ){
+ 		return index;
+ 	}else if(target > array[index]){
+ 		for(var i=array[index]+1 ; i<array.length ; i++){
+ 			if(target === array[i]){
+ 				return i;
+ 			}
+ 		}
+ 	}else{
+ 		for(var i=0 ; i<index ; i++){
+ 			if(target === array[i]){
+ 				return i;
+ 			}
+ 		}
+ 	}
+ 	return "not found";  
+ };
 //O(log(n))
 
