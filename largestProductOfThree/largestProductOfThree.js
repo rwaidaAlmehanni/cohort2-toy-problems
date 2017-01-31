adac7e4682b60c87502828079994fcf48eff3ec3
rwaida <rwaidaAlmehanni>
2017-01-31 08:43:04 +0200

    pro3

---
+++ largestProductOfThree/largestProductOfThree.js
@@ -0,0 +1,22 @@
+/* Write a function that finds the largest possible product of any three numbers
+ * from an array.
+ * 
+ * Example:
+ * largestProductOfThree([2, 1, 3, 7]) === 42
+ *
+ * Extra credit: Make your function handle negative numbers.
+ */
+
+var largestProductOfThree = function(array) {
+	var result=1;
+      array=array.sort();//O(n)
+   for(var i=array.length-1; i>=array.length-3; i--){//O(1)
+         result*=array[i];
+      }
+      return result;
+};
+//O(n)
+
+
+
+
